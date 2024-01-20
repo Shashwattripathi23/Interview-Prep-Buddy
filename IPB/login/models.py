d52e42a1b74e7ccd11795eb9e75780e4b8cf3715
Shashwattripathi23 <tripathishashwat2309@gmail.com>
2024-01-20 22:48:50 +0530

    addded

--- IPB/login/models.py
+++
@@ -1,8 +0,0 @@
-from django.db import models
-
-# Create your models here.
-
-
-class details(models.Model):
-    username = models.CharField(max_length=15)
-    pas = models.CharField(max_length=15)
